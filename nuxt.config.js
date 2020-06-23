cdbac8152dbf2d70fe501a41534f27bb588c51bb
Tom Dyson <tom@torchbox.com>
2020-06-23 14:56:09 +0100

    font reference typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,7 +22,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans|Source+Serif+Pro' }
     ]
   },
