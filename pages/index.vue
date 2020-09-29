113e92135cfadf64e986ac375af67bb0e6f975e2
Tom Dyson <tom@torchbox.com>
2020-09-29 11:35:26 +0100

    Restrict index listing to 'show in menu' pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
   <div class="container sm:px-16 px-6 pt-8 max-w-5xl">
     <p v-if="$fetchState.pending">Fetching posts...</p>
-    <p v-else-if="$fetchState.error">Error while fetching posts: {{ $fetchState.error.message }}</p>
+    <p v-else-if="$fetchState.error">
+      Error while fetching posts: {{ $fetchState.error.message }}
+    </p>
     <ul v-else>
       <li v-for="post of posts.items" :key="post.id" class="pb-4">
         <n-link
           :to="`/${post.meta.slug}`"
           class="text-5xl avenir font-bold leading-none text-gray-800 pb-6"
-        >{{ post.title }}</n-link>
-        <p class="date pt-0 pb-4 athelas font-light text-red-400">{{ post.date|datify }}</p>
+          >{{ post.title }}</n-link
+        >
+        <p class="date pt-0 pb-4 athelas font-light text-red-400">
+          {{ post.date | datify }}
+        </p>
       </li>
     </ul>
   </div>
@@ -35,7 +40,7 @@ export default {
   },
   async fetch() {
     this.posts = await this.$axios.$get(
-      `${process.env.baseApiUrl}/pages/?type=blog.BlogPage&fields=title,date`
+      `${process.env.baseApiUrl}/pages/?type=blog.BlogPage&fields=title,date&show_in_menus=true`
     );
   },
   filters: {
